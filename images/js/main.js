2ea1324760ec62de345cf6ea40c9f800fbdcc79f
pritee6594 <57859457+pritee6594@users.noreply.github.com>
2020-12-10 11:10:02 +0000

    Add files via upload

---
+++ images/js/main.js
@@ -0,0 +1,213 @@
+
+      $(document).ready(function () {
+        $('.sidenav').sidenav();
+      });
+    
+      $(document).ready(function () {
+        $('.slider').slider();
+      });
+
+      (function ($) {
+
+        $(function () { // ON DOM READY
+
+
+          var data = {
+            "response": {
+              "docs": [{
+			
+                "content_type": "article",
+                "date": "2011",
+				"rating":"4.5",
+				"prise":"500cr",
+				"cost":"450cr",
+				"director":"Ivan Mitov",
+                "description": "A UFO is shot down over Russia in the 1970s and the wreckage is taken to a secret research facility. When the Soviet Union collapses, the alien technology falls into the hands of a mysterious terrorist organisation, which uses it to create a devastating doomsday weapon. A team of secret agents is sent to stop the carnage.",
+                "image_url":[
+                  "images\/a-1.jpg"],
+                "title": "Cold Fusion",
+                "url": ["trailerrr.mp4"]
+              }, {
+				
+                "content_type": "article",
+                "date": "2016",
+				"rating":"3.5",
+				"prise":"300cr",
+				"cost":"250cr",
+				"director":"Salman Khan",
+                "description": "Kick is a 2014 Indian Hindi-language action heist film produced and directed by Sajid Nadiadwala, and starring Salman Khan, Jacqueline Fernandez and Randeep Hooda in prominent roles, with Mithun Chakraborty, Nawazuddin Siddiqui, Saurabh Shukla and Archana Puran Singh portraying other important characters.",
+                "image_url": [
+                  "images\/a-2.jpg"],
+                "title": "Kick",
+                "url": ["kick.mp4"]
+              },
+              {
+                "content_type": "article",
+                "date": "2011",
+				"rating":"4.0",
+				"prise":"200cr",
+				"cost":"150cr",
+				"director":"Kenneth Branagh",
+                "description": "Thor is a 2011 American superhero film based on the Marvel Comics character of the same name. Produced by Marvel Studios and distributed by Paramount Pictures,[N 1] it is the fourth film in the Marvel Cinematic Universe (MCU).",
+                "image_url": [
+                  "images\/a-3.jpg"],
+                "title": "Thor",
+                "url": ["Thor.mp4"]
+              },
+              {
+                "content_type": "article",
+                "date": "1995",
+				"rating":"5.0",
+				"prise":"400cr",
+				"cost":"350cr",
+				"director":"Richard Linklater",
+                "description": "The film follows Jesse (Ethan Hawke), a young American man, and Céline (Julie Delpy), a young French woman, who meet on a train and disembark in Vienna, where they spend the night walking around the city and getting to know and falling in love with each other.",
+                "image_url": [
+                  "images\/a-6.jpg"],
+                "title": "Before Sunrise",
+                "url": ["Before Sunrise.mp4"]
+              },
+              {
+                "content_type": "article",
+                "date": "2020",
+				"rating":"7.0",
+				"prise":"800cr",
+				"cost":"400cr",
+				"director":"Yeon Sang-ho",
+                "description": "It is a standalone sequel to the 2016 film Train to Busan and follows a soldier who is sent along with his team to retrieve a truck full of money from the wastelands of the Korean peninsula now inhabited by zombies, rogue militia, and a family.",
+                "image_url": [
+                  "images\/a-7.jpg"],
+                "title": "Peninsula",
+                "url": ["Peninsula.mp4"]
+              }, {
+                "content_type": "article",
+                "date": "2013",
+				"rating":"8.0",
+				"prise":"600cr",
+				"cost":"500cr",
+				"director":"J.J. Abrams",	
+                "description": "Star Trek Into Darkness is a 2013 American science fiction action film directed by J. J. Abrams and written by Roberto Orci, Alex Kurtzman, and Damon Lindelof.",
+                "image_url": [
+                  "images\/movie1.jpg"],
+                "title": "Star trek into darkness",
+                "url": ["Darkness.mp4"]
+              },
+              {
+                "content_type": "article",
+                "date": "2016",
+				"rating":"6.0",
+				"prise":"500cr",
+				"cost":"400",
+				"director":"David Yates",
+                "description": "Fantastic Beasts and Where to Find Them (or simply Fantastic Beasts) is the first prequel of a series of films based on Fantastic Beasts and Where to Find Them, one of the companion books to J. K.",
+                "image_url": [
+                  "images\/movie2.jpg"],
+                "title": "Fantastic Beasts",
+                "url": ["Fantastic Beasts.mp4"]
+              },
+              {
+                "content_type": "article",
+                "date": "2016",
+				"rating":"9.0",
+				"prise":"100cr",
+				"cost":"600",
+				"director":"Gareth Edwards",
+                "description": "The film premiered in Los Angeles on December 10, 2016, and was released in the United States on December 16.",
+                "image_url": [
+                  "images\/movie7.jpg"],
+                "title": "Rogue One",
+                "url": ["Rogue One.mp4"]
+              }, {
+                "content_type": "article",
+                "date": "2014",
+				"rating":"3.0",
+				"prise":"800cr",
+				"cost":"400",
+				"director":"A.R. Murugadoss",
+                "description": "A military officer uses his intellect and fighting skills to hunt and knock down a terrorist aiming to rip apart Mumbai via the sleeper cells under his command.",
+                "image_url": [
+                  "images\/pic6.jpg"],
+                "title": "Holiday",
+                "url": ["Holiday.mp4"]
+              }, {
+                "content_type": "article",
+                "date": "2020",
+				"rating":"6.0",
+				"prise":"500cr",
+				"cost":"400",
+				"director":"Joel Crawford",
+                "description": "A cave family called the Croods survives a natural disaster, due to the overprotective nature of their stubborn patriarch Grug.",
+                "image_url": [
+                  "images\/pic3.jpg"],
+                "title": "The Croods",
+                "url": ["The Croods.mp4"]
+              }, {
+                "content_type": "article",
+                "date": "2016",
+				"rating":"7.0",
+				"prise":"900cr",
+				"cost":"400",
+				"director":"Justin Lin",
+                "description": "Star Trek Beyond is a 2016 American science fiction action film directed by Justin Lin, written by Simon Pegg and Doug Jung, and based on the television series Star Trek created by Gene Roddenberry.",
+                "image_url": [
+                  "images\/pic7.jpg"],
+                "title": "Star trek Beyond",
+                "url": ["Star Trek Beyond.mp4"]
+              },
+
+
+              {
+                "content_type": "article",
+                "date": "1970",
+				"rating":"6.0",
+				"prise":"500cr",
+				"cost":"400",
+				"director":"Christopher Nolan",
+                "description": "Inception is a 2010 science fiction action film[4][5][6] written and directed by Christopher Nolan, who also produced the film with his wife, Emma Thomas. The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets.",
+                "image_url": [
+                  "images\/m-3.jpg"],
+                "native_id": "",
+                "title": "Inception",
+                "url": ["Inception.mp4"]
+              }]
+            }
+          }, data = data.response.docs,
+            target = $('#target'),
+            html;
+
+          $.each(data, function (key, val) {
+            html = '<div class="column"><div class="card"><div class="image-list nn">';
+            html += '<img src ="' + val.image_url + '" class="image-styles" width="100%" />';
+            html += '<p class="image-title">' + val.title + '</p>';
+            html += '</div></div></div>';
+            target.append(html);
+          });
+          $("#myInput").keyup(function () {
+            var value = this.value.toLowerCase();
+            $(".column").each(function (index) {
+              var id = $(this).find(".image-title").text().toLowerCase();
+              console.log(id);
+              $(this).toggle(id.indexOf(value) !== -1);
+            });
+          });
+		  
+		  
+		  $('div.nn').click(function(){
+			
+			var r=data.findIndex((element, index)=>{
+					
+					var id = $(this).find(".image-title").text();
+					if(element.title===id){
+					return true
+					}
+			});
+			//console.log(r+1);
+			sessionStorage.setItem('TheArray',JSON.stringify(data[r]));
+			window.location.href='page2.html';
+		});
+
+        }); // end of on DOM READY
+
+
+      }(jQuery));
+    
